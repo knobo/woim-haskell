35544ca941038589324989d8136209e97dc80e66
Knut Olav Bøhmer <bohmer@gmail.com>
2009-12-19 09:19:48 +0100

    Print in graphwiz dot format.

--- woim.hs
+++ woim.hs
@@ -27,56 +27,98 @@ ismLine n ((na, xs):ys) = na == n + 2
 parseLines :: Tokens String -> Tokens WoimItem
 parseLines [] = []
 parseLines ((n, xs):ys) 
-     | ismLine n ys = let (accumulated, rest) = parseMultiLines (n + 2) ys
-                      in  (n, WoimMultiLine  (xs:accumulated)) : parseLines rest
-     | otherwise    = (n, WoimLine xs) : parseLines ys
+  | ismLine n ys = let (accumulated, rest) = parseMultiLines (n + 2) ys
+                   in  (n, WoimMultiLine  (xs:accumulated)) : parseLines rest
+  | otherwise    = (n, WoimLine xs) : parseLines ys
 
 -- When multiline occurs, collect all lines on that level.
 parseMultiLines :: Integer -> Tokens String -> ([String], Tokens String)
 parseMultiLines n [] = ([],[])
 parseMultiLines n t@((nx, xs):ys) 
-    | nx == n   = let (txt, rest) = parseMultiLines nx ys
-                  in ((xs:txt), rest)
-    | otherwise = ([], t)
+  | nx == n   = let (txt, rest) = parseMultiLines nx ys
+                in ((xs:txt), rest)
+  | otherwise = ([], t)
 
 buildTree :: Integer -> Tokens WoimItem -> (Tokens WoimItem, WoimList)
 buildTree _ [] = ([],[])
 buildTree n xxs@((level,x):xs)
-    | level < n  = (xxs, [])
-    | otherwise  = let (besides, children) = buildTree (level+8) xs       -- do we have children? if no level < n
-                       (uncles, siblings)  = buildTree level besides      -- get my (level,x) brothers
-                   in (uncles, Woim x children : siblings)
+  | level < n  = (xxs, [])
+  | otherwise  = let (besides, children) = buildTree (level+8) xs       -- do we have children? if no level < n
+                     (uncles, siblings)  = buildTree level besides      -- get my (level,x) brothers
+                 in (uncles, Woim x children : siblings)
 
 printTabs n = putStr $ replicate n '\t'
 
 printTree :: Int -> WoimList -> IO()
-printTree n [] = putStr ""
-printTree n ((Woim (WoimLine txt) sub):rest) = do 
-        printTabs n
-        putStrLn txt
-        printTree (n + 1) sub
-        printTree n rest
-printTree n ((Woim (WoimMultiLine (x:xs)) sub):rest) = do 
-        printTabs n
-        putStrLn x
-        printMultiLineList n xs
-        printTree (n + 1) sub
-        printTree n rest
+printTree n [] = return ()
+printTree n ((Woim (WoimLine txt) children):siblings) = do 
+  printTabs n
+  putStrLn txt
+  printTree (n + 1) children
+  printTree n siblings
+printTree n ((Woim (WoimMultiLine (x:xs)) children):siblings) = do 
+  printTabs n
+  putStrLn x                             -- First line as normal
+  printMultiLineList n xs                -- Rest with +2 spaces
+  printTree (n + 1) children
+  printTree n siblings
 
 printMultiLineList :: Int -> [String] -> IO()
-printMultiLineList n [] = putStr "";
+printMultiLineList n [] = return ()
 printMultiLineList n (x:xs) = 
-     do printTabs n
-        putStr "  "
-        putStrLn x
-        printMultiLineList n xs
+  do printTabs n
+     putStr "  "
+     putStrLn x
+     printMultiLineList n xs
 
 getTree :: (Tokens WoimItem, WoimList) -> WoimList
-getTree (_,woim) = woim
+getTree = snd
 
 main = do 
   woimLines <- lines `liftM` readFile "/home/knobo/prog/haskell/woim/woim.woim"
-  let tokens = parseLines $ splitAll woimLines in
-      printTree 0 $ getTree $ buildTree (-1) tokens
+  let tokens = parseLines $ splitAll woimLines 
+    in printDot $ getTree $ buildTree (-1) tokens
+ -- printTree 0 $ getTree $ buildTree (-1) tokens
 
+printBackPointer :: String -> IO()
+printBackPointer [] = return ()
+printBackPointer (x:[]) = return ()
+printBackPointer (x:xs) = 
+  do mapM_ putStr [xs, " -> ", (x:xs), ";\n" ]
+
+labelFormat (WoimMultiLine []) = []
+labelFormat (WoimMultiLine (x:xs)) = do 
+  quoteLabel x ++ labelFormat (WoimMultiLine xs)
+labelFormat (WoimLine x) = do 
+  quoteLabel x
+
+quoteLabel :: String -> String
+quoteLabel [] = []
+quoteLabel (x:xs) = do
+  (case x of
+    '\"'      ->  "\\\""
+    '\\'      ->  "\\"
+    '\n'      ->  "\\n"
+    otherwise ->  [x])  ++  quoteLabel xs
+
+printDotItems :: String -> WoimList -> IO()
+printDotItems _ [] = return ()
+printDotItems id@(i:is) woimlist@((Woim x children):siblings) = 
+  do mapM_ putStr [id, " [label=\"", labelFormat x, "\"];\n"]
+     printBackPointer id
+     head [ printDotItems (newid:id) children | newid <- ['a'..] ]
+     printDotItems ((succ i):is) siblings
+
+printDot :: WoimList -> IO()
+printDot woim = do
+  putStrLn "digraph G {"
+  head [ printDotItems [id] woim | id <- ['a'..] ]
+  putStrLn "}"
+
+withFile :: FilePath -> IOMode -> ( Handle -> IO a ) -> IO a
+withFile path mode f = do
+  handle <- openFile path mode
+  result <- f handle
+  hClose handle
+  return result
 
